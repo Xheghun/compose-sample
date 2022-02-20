81a4b1eebde8263cf23adc0975190a46146a5999
david <davidr@pointzi.com>
2022-02-20 22:26:13 +0100

    style and theming

--- app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
+++ app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.xheghun.compsecodelabsample
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.xheghun.compsecodelabsample.ui.theme.CompseCodelabSampleTheme
@@ -44,7 +46,7 @@ fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text("Hello,")
-                Text(name)
+                Text(name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
             }
             OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(if (expanded) "Show less" else "Show more")
@@ -62,6 +64,11 @@ fun Greetings(names: List<String> = List(1000){"$it"}) {
     }
 }
 
+
+@Preview(showBackground = true,
+widthDp = 320,
+uiMode = UI_MODE_NIGHT_YES,
+name = "De")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
