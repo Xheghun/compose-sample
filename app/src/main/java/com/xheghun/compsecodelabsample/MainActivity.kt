d279ad4bc587d2088d01f21e153659edbdf1a235
david <davidr@pointzi.com>
2022-02-20 11:41:10 +0100

    ...

--- app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
+++ app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.xheghun.compsecodelabsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -27,12 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(modifier = Modifier.padding(24.dp).fillMaxWidth()) {
+            Text("Hello,")
+            Text(name)
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     CompseCodelabSampleTheme {
@@ -41,8 +46,10 @@ fun DefaultPreview() {
 }
 
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 5.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
