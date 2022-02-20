c38a5cb42238d92fc91b73e0fee77396c68a357a
david <davidr@pointzi.com>
2022-02-20 12:34:01 +0100

    managing states

--- app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
+++ app/src/main/java/com/xheghun/compsecodelabsample/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,14 +29,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier.weight(1f)) {
                 Text("Hello,")
                 Text(name)
+                if(expanded.value)
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(45.dp))
             }
-            OutlinedButton(onClick = { }) {
-                Text("Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
